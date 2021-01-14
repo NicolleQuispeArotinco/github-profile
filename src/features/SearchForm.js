0f8a4cd317681320138943e62be7e2d9b132eabf
NicolleQuispeArotinco <nicolleqa3@gmail.com>
2021-01-14 16:15:19 -0500

    added search icon

--- src/features/SearchForm.js
+++ src/features/SearchForm.js
@@ -1,9 +1,20 @@
 import React from "react";
 
+import { Formik, Form, Field} from "formik";
+
+import searchIcon from "../images/searchIcon.svg";
 function SearchForm(){
   return(
     <div>
       <h1>Seach Form</h1>
+      <Formik>
+        <Form>
+          <Field type="text" />
+          <button>
+            <img src={searchIcon}/>
+          </button>
+        </Form>
+      </Formik>
     </div>
   )
 }
